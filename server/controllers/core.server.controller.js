430fccdfe51423b686d7f4b3f0da703c1ced1db3
Olivier <devangularjs@gmail.com>
2017-05-25 20:46:13 +0200

    feat(user middleware): Ajout de la fonctionnalité

---
+++ server/controllers/core.server.controller.js
@@ -0,0 +1,33 @@
+// Déclaration des librairies nodeJS
+const path = require('path');
+
+// Déclaration des librairies
+const validator = require('validator');
+
+// Déclaration des fichiers de configuration
+const config = require(path.resolve('./config/config'));
+
+exports.renderIndexHTML = function (req, res, next) {
+  const safeUserObject = null;
+
+  // Création de l'objet safeUserObject si le user existe
+  if (req.user) {
+    safeUserObject = {
+      displayName: validator.escape(req.user.displayName),
+      provider: validator.escape(req.user.provider),
+      created: req.user.created.toString(),
+      roles: req.user.roles,
+      email: validator.escape(req.user.email),
+      lastname: validator.escape(req.user.lastname),
+      firstname: validator.escape(req.user.firstname),
+      phone: validator.escape(req.user.phone),
+      additionalProvidersData: req.user.additionalProvidersData
+    };
+  }
+
+  // Affichage du fichier index par défaut pour toutes les routes
+  res.render(path.resolve('./dist/index.html'), {
+    user: JSON.stringify(safeUserObject),
+    sharedConfig: JSON.stringify(config.shared)
+  });
+};
