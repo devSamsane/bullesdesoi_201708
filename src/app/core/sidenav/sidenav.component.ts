47e4861d13c990d2ec2f69bca3a42384bf924a88
Olivier <devangularjs@gmail.com>
2017-06-05 14:25:27 +0200

    feat(sidenav): mise à jour du component - Ajout logo - Ajout du theme sidenav - Ajout background

--- src/app/core/sidenav/sidenav.component.ts
+++ src/app/core/sidenav/sidenav.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
 @Component({
   selector: 'bds-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css'],
+  styleUrls: ['./sidenav.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class SidenavComponent implements OnInit {
